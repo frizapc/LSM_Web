2e535d73973b06432881bd5ccb4df3634bd0e66d
frizapc <frizamuhammad99@gmail.com>
2025-12-14 12:08:23 +0700

    feat(view): add load time in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,9 +2,10 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col-md-6">
-                <span class="text-white">© {{ date('Y') }} Muhammad Friza - 220511017</span>
+                <span class="text-white">© {{ date('Y') }} Miracle Sender</span>
             </div>
             <div class="col-md-6 text-md-end">
+                <span id="load-time"></span>
                 <a href="#" class="text-white me-3"><i class="bi bi-shield-lock"></i> Privacy</a>
                 <a href="#" class="text-white"><i class="bi bi-question-circle"></i> Help</a>
             </div>
